553d23452bf9789a56502b46ecc6edae71156771
wuchengxing <1449697757@qq.com>
2021-08-16 01:02:15 +0800

    feat：字节码文件修改

--- chapter01/src/com/atguigu/java1/ArrayTest.java
+++ chapter01/src/com/atguigu/java1/ArrayTest.java
@@ -7,12 +7,15 @@ package com.atguigu.java1;
 public class ArrayTest {
     public static void main(String[] args) {
         Object[] arr = new Object[10];
-        System.out.println(arr);//[Ljava.lang.Object;@1540e19d
+        //[Ljava.lang.Object;@1540e19d
+        System.out.println(arr);
 
+        //[Ljava.lang.String;@677327b6
         String[] arr1 = new String[10];
-        System.out.println(arr1);//[Ljava.lang.String;@677327b6
+        System.out.println(arr1);
 
+        //[[J@14ae5a5
         long[][] arr2 = new long[10][];
-        System.out.println(arr2);//[[J@14ae5a5
+        System.out.println(arr2);
     }
 }
