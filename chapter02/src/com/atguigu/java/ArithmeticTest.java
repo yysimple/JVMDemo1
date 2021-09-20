f328e6f50d5875d54a6e7cc21f8e3e23d617f1a4
wuchengxing <1449697757@qq.com>
2021-09-20 11:56:19 +0800

    feat：测试左移

--- chapter02/src/com/atguigu/java/ArithmeticTest.java
+++ chapter02/src/com/atguigu/java/ArithmeticTest.java
@@ -75,6 +75,14 @@ public class ArithmeticTest {
         return 0;
     }
 
+    @Test
+    public void bitMove(){
+        int a = 1;
+        int b = 2;
+        int c = a << b;
+        System.out.println(c);
+    }
+
     /**
      * 关于(前)++和(后)++
      */
